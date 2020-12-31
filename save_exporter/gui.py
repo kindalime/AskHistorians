ca6d7667c91c45f644f019e91939bb6881abd543
Daniel Kim <danielynskim@gmail.com>
2020-12-30 21:10:13 -0500

    Further develop GUI and differentiate pages

--- save_exporter/gui.py
+++ save_exporter/gui.py
@@ -9,20 +9,36 @@ from tkcalendar import DateEntry
 class Page(tk.Frame):
     def __init__(self, *args, **kwargs):
         tk.Frame.__init__(self, *args, **kwargs)
-        self.label = tk.Label(self, text="This is page 1")
-        self.label.pack(side="top", fill="both", expand=True)
-
-class CredentialInput(tk.Frame):
-    pass
-
-class CalendarSelect(tk.Frame):
-    def __init__(self, *args, **kwargs):
-        tk.Frame.__init__(self, *args, **kwargs)
         self.labels = []
+    
+    def add_label(self, text):
+        self.label = tk.Label(self, text=text)
+        self.label.pack(padx=10, pady=10)
+    
+    def change_label(self, text, num=0):
+        self.labels[num]["text"] = text
+
+class CredentialInput(Page):
+    def __init__(self, *args, **kwargs):
+        Page.__init__(self, *args, **kwargs)
+        self.entries = []
+    
+    def add_entry(self, text):
+        label = tk.Label(self, text=text)
+        label.pack(padx=10, pady=10)
+        self.labels.append(label)
+
+        cal = tk.Entry(self)
+        cal.pack(padx=10, pady=10)
+        self.entries.append(cal)
+
+class CalendarSelect(Page):
+    def __init__(self, *args, **kwargs):
+        Page.__init__(self, *args, **kwargs)
         self.calendars = []
     
-    def add_calendar(self):
-        label = ttk.Label(self, text="From Date")
+    def add_calendar(self, text):
+        label = tk.Label(self, text=text)
         label.pack(padx=10, pady=10)
         self.labels.append(label)
 
@@ -33,9 +49,15 @@ class CalendarSelect(tk.Frame):
 class SaveFetcherGUI(tk.Frame):
     def __init__(self, *args, **kwargs):
         tk.Frame.__init__(self, *args, **kwargs)
-        self.pages = [Page(self), CalendarSelect(self), Page(self)]
-        self.pages[1].add_calendar()
-        self.pages[1].add_calendar()
+        self.pages = [CredentialInput(self), CalendarSelect(self), Page(self)]
+        self.page_funcs = []
+
+        self.pages[0].add_entry("Username")
+        self.pages[0].add_entry("Password")
+        self.pages[1].add_calendar("From Date")
+        self.pages[1].add_calendar("To Date")
+        self.pages[2].add_label("Currently working...")
+
         self.current_page = 0
         self.setup_gui()
 
@@ -61,8 +83,9 @@ class SaveFetcherGUI(tk.Frame):
 
     def next_page(self):
         if self.current_page + 1 < len(self.pages):
-            self.current_page += 1
-            self.pages[self.current_page].lift()
+            if self.page_funcs[self.current_page]():
+                self.current_page += 1
+                self.pages[self.current_page].lift()
 
 if __name__ == "__main__":
     root = tk.Tk()
