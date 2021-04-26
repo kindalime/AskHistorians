1eb4bcbfa1029b5ba0fb884439f421b4c02a7915
Daniel Kim <danielynskim@gmail.com>
2021-04-26 19:00:09 -0400

    Quick update to message sender

--- message_sender/message_sender.py
+++ message_sender/message_sender.py
@@ -10,10 +10,13 @@ password = os.getenv("PASSWORD")
 client_id = os.getenv("CLIENTID")
 client_secret = os.getenv("CLIENTSECRET")
 user_agent = "MessageSender:v1.0 (by u/AverageAngryPeasant)"
-reddit = praw.Reddit(client_id=client_id, client_secret=client_secret, user_agent=user_agent, username=username, password=password)
+reddit = praw.Reddit(client_id=client_id, client_secret=client_secret, user_agent=user_agent, username=username, password=password, ratelimit_seconds=60)
 
 with open('messages.csv', "r") as csvfile:
     reader = csv.reader(csvfile)
     for row in reader:
         recipient, subject, message = row
-        reddit.redditor(recipient).message(subject, message)
+        try:
+            reddit.redditor(recipient).message(subject, message)
+        except (praw.exceptions.RedditAPIException) as e:
+            print(e.error_type == 'NOT_WHITELISTED_BY_USER_MESSAGE')
